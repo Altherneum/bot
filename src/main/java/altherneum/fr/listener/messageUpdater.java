d1ce326faa2a1c9570c288cdb44e6b025e0255a8
lx78WyY0J5 <9j2k37st@gmail.com>
2024-04-16 17:40:06 +0200

    Update messageUpdater.java

--- src/main/java/altherneum/fr/listener/messageUpdater.java
+++ src/main/java/altherneum/fr/listener/messageUpdater.java
@@ -1,9 +1,11 @@
+package altherneum.fr.listener;
+
 //TDL auto update msg based on channel id, msg id, and message
 // if msg == null get channel id & post it
 //Async thread 
 
 
-public class messageUpdater(){
+public class messageUpdater{
 
 public void sendUpdate(int channel, int messageID, string message){
 }
