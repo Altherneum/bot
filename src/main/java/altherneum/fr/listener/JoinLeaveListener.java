86be9b93cfb284507caf041b237112cb489f472a
lx78WyY0J5 <9j2k37st@gmail.com>
2026-01-29 21:44:52 +0100

    also on leave

--- src/main/java/altherneum/fr/listener/JoinLeaveListener.java
+++ src/main/java/altherneum/fr/listener/JoinLeaveListener.java
@@ -35,8 +35,9 @@ public class JoinLeaveListener {
                 EmbedBuilder embedBuilder = new EmbedBuilder();
                 embedBuilder.setThumbnail(user.getAvatar());
                 embedBuilder.setTitle("\uD83D\uDEAB Aurevoir");
-                embedBuilder.setDescription(user.getMentionTag() + "  (`" + user.getDiscriminatedName()
-                        + "`)\nSur Discord depuis le : **__" + StatsTimer.DateFormated(date2) + "__**");
+                embedBuilder.setDescription(user.getMentionTag() + "  (`" + user.getDiscriminatedName() + "`)"
+                        + "\nSur Discord depuis le : **__" + StatsTimer.DateFormated(date2) + "__**"
+                        + "\nIdentifiant unique : `" + user.getIdAsString() + "`");
                 embedBuilder.addInlineField("Messages", "" + i);
                 embedBuilder.addInlineField("Invitations", "" + total);
                 main.api.getChannelById(IDs.Invitation).get().asServerTextChannel().get().sendMessage(embedBuilder);
